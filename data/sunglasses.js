e3347dff08d11380f3501a4243011bfe471199c1
McJafet <jafet2195@gmail.com>
2024-08-20 15:48:04 -0500

    separated self-contained data objects

---
+++ data/sunglasses.js
@@ -0,0 +1,24 @@
+export const sunglasses = {
+    model: {
+        name: "aviator",
+        price: 300,
+        thumbImg: "./images/thumb-aviator.png",
+        cssClass: "frame-aviator",
+    },
+    lenses: {
+        color: "sepia",
+        price: 20,
+        cssClass: "color-sepia",
+    },
+    frame: {
+        color: "charcoal",
+        price: 0,
+        cssClass: "color-charcoal",
+    }     
+}
+
+export function setSunglasses(sunglassesNew = sunglasses) {
+    return sunglassesNew
+}
+
+
--- index.js
+++ index.js
@@ -1,84 +1,5 @@
-const sunglassesOptions = {
-    models: [
-        {
-            name: "aviator",
-            price: 300,
-            thumbImg: "thumb-aviator.png",
-            cssClass: "frame-aviator",
-        },
-        {      
-            name: "half-frame",
-            price: 200,
-            thumbImg: "thumb-half-frame.png",
-            cssClass: "frame-half",
-        },
-        {
-            name: "round",  
-            price: 250,
-            thumbImg: "thumb-round.png",
-            cssClass: "frame-round",
-        },
-        {  
-            name: "wayfarer",
-            price: 250,
-            thumbImg: "thumb-wayfarer.png",
-            cssClass: "frame-wayfarer",
-        }],
-    lenses: [
-        {
-            color: "sepia",
-            price: 20,
-            cssClass: "color-sepia",
-        },
-        {
-            color: "rainbow",
-            price: 50,
-            cssClass: "color-rainbow",
-        },
-        {
-            color: "iridescent",
-            price: 30,
-            cssClass: "color-iridescent",
-        }],
-    frames: [
-        {
-            color: "charcoal",
-            price: 0,
-            cssClass: "color-charcoal",
-        },
-        {
-            color: "tan",
-            price: 0,
-            cssClass: "color-tan",
-        },
-        {
-            color: "rose",
-            price: 0,
-            cssClass: "color-rose",
-        },
-    ],
-}
-
-const sunglasses = {
-    model: {
-        name: "aviator",
-        price: 300,
-        thumbImg: "./images/thumb-aviator.png",
-        cssClass: "frame-aviator",
-    },
-    lenses: {
-        color: "sepia",
-        price: 20,
-        cssClass: "color-sepia",
-    },
-    frame: {
-        color: "charcoal",
-        price: 0,
-        cssClass: "color-charcoal",
-    }     
-}
-
-
+import { sunglasses, setSunglasses } from "./data/sunglasses.js";
+import { sunglassesOptions } from "./data/sunglassesOptions.js";
 
 const productDetailsEl = document.getElementById("productDetails")
 const productImage = document.getElementById("productImage")
@@ -86,10 +7,6 @@ const productFrames = document.getElementsByClassName("product-image_frame")[0]
 const productLenses = document.getElementsByClassName("product-image_lenses")[0]
 
 let sunglassesNew = ''
-
-function setSunglasses(sunglassesNew = sunglasses) {
-    return sunglassesNew
-}
 
 function render(sunglassesNew) {
     
